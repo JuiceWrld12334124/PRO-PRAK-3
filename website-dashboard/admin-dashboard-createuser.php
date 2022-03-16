49b2a03249f2dafd697a4b511c138ba0ec40533c
JuiceWrld12334124 <vgeance50@gmail.com>
2022-03-16 15:09:46 +0100

    Sidenav icon fix

--- website-dashboard/admin-dashboard-createuser.php
+++ website-dashboard/admin-dashboard-createuser.php
@@ -1,121 +1,137 @@
 <?php
-    session_start();
-    if($_SESSION['userrole'] !="admin"){
-        header("location:../index.php");
-    }
+session_start();
+if ($_SESSION['userrole'] != "admin") {
+    header("location:../index.php");
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <html>
-  <head>
-  <meta charset="UTF-8">
+<head>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/Framework.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
-  </head>
-  <body>
+</head>
+<body>
 
-  <section class="overons-head bg-primary">
-        <div class="container grid grid-1 absolute">
-            <div>
-                <h1 class="text-center xl">Admin - users</h1>
-            </div>
-            <img src="./img/programming-code.svg" alt="">
+<section class="overons-head bg-primary">
+    <div class="container grid grid-1 absolute">
+        <div>
+            <h1 class="text-center xl">Admin - users</h1>
         </div>
-    </section>
-      
-  <div class="area">
-      
+        <img src="./img/programming-code.svg" alt="">
+    </div>
+</section>
 
-  </div>
-  
-  <nav class="main-menu">
-            <ul>
-                <li>
-                    <a href="./admin-dashboard.php">
-                        <i class="fa fa-home"></i>
-                        <span class="nav-text">
+<div class="area">
+
+
+</div>
+
+<nav class="main-menu">
+    <ul>
+        <li>
+            <a href="./admin-dashboard.php">
+                <i class="fa fa-home fa-2x"></i>
+                <span class="nav-text">
                             Users
                         </span>
-                    </a>
-                  
-                </li>
-                <li class="has-subnav">
-                    <a href="./admin-postpage.php">
-                        <i class="fa fa-laptop fa-2x"></i>
-                        <span class="nav-text">
+            </a>
+
+        </li>
+        <li class="has-subnav">
+            <a href="./admin-postpage.php">
+                <i class="fa fa-clone fa-2x"></i>
+                <span class="nav-text">
                             Posts
                         </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="./admin-dashboard-update-roles.php">
-                       <i class="fa fa-list fa-2x"></i>
-                        <span class="nav-text">
+            </a>
+
+        </li>
+        <li class="has-subnav">
+            <a href="./admin-dashboard-update-roles.php">
+                <i class="fa fa-list fa-2x"></i>
+                <span class="nav-text">
                             Roles
                         </span>
-                    </a>
-                    
-                </li>
+            </a>
 
-                <li class="has-subnav">
-                    <a href="./admin-dashboard-banappeals.php">
-                       <i class="fa fa-list fa-2x"></i>
-                        <span class="nav-text">
+        </li>
+
+        <li class="has-subnav">
+            <a href="./admin-dashboard-banappeals.php">
+                <i class="fa fa-ban fa-2x"></i>
+                <span class="nav-text">
                             Ban appeals
                         </span>
-                    </a>
-                </li>
+            </a>
+        </li>
 
-                <li class="has-subnav">
-                    <a href="./admin-dashboard-createuser.php">
-                       <i class="fa fa-list fa-2x"></i>
-                        <span class="nav-text">
+        <li class="has-subnav">
+            <a href="./admin-dashboard-createuser.php">
+                <i class="fa fa-plus fa-2x"></i>
+                <span class="nav-text">
                             Create user
                         </span>
-                    </a>
-                </li>
+            </a>
+        </li>
 
-            </ul>
+        <li class="has-subnav">
+            <a href="./admin-acceptuser.php">
+                <i class="fa fa-user-plus fa-2x"></i>
+                <span class="nav-text">
+                            Applications
+                        </span>
+            </a>
+        </li>
 
-            <ul class="logout">
-                <li>
-                   <a href="/api/logout.php">
-                         <i class="fa fa-power-off fa-2x"></i>
-                        <span class="nav-text">
+    </ul>
+
+    <ul class="logout">
+        <li>
+            <a href="/api/logout.php">
+                <i class="fa fa-power-off fa-2x"></i>
+                <span class="nav-text">
                             Logout
                         </span>
-                    </a>
-                </li>  
-            </ul>
-        </nav>
+            </a>
+        </li>
+    </ul>
+</nav>
 
-        <section class="review-head ">
-        <div class="container grid grid-1">
-            <div class="">
-                <form class="form-loginPage" 
-            id="review_submit" method="post" action="/api/register-script.php"
+<section class="review-head ">
+    <div class="container grid grid-1">
+        <div class="">
+            <form class="form-loginPage"
+                  id="review_submit" method="post" action="/api/register-script.php"
                   onsubmit="return passwordValidation(); window.location.reload()">
                 <h2 class="h2">Create user</h2>
                 <div class="form-control">
-                    <p class="p" for="username" type="Naam:"><input required class="input" name="username" placeholder="Gebruikersnaam"></input></p>
+                    <p class="p" for="username" type="Naam:"><input required class="input" name="username"
+                                                                    placeholder="Gebruikersnaam"></input></p>
                 </div>
                 <div class="form-control">
-                    <p class="p" for="password" type="Wachtwoord"><input type="password" required class="input" name="password" placeholder="Wachtwoord"></input></p>
+                    <p class="p" for="password" type="Wachtwoord"><input type="password" required class="input"
+                                                                         name="password"
+                                                                         placeholder="Wachtwoord"></input></p>
                 </div>
                 <div class="form-control">
-                    <p class="p" for="password" type="Opnieuw wachtwoord"><input type="password" name="password_confirmation" required class="input" name="password" placeholder="Wachtwoord"></input></p>
+                    <p class="p" for="password" type="Opnieuw wachtwoord"><input type="password"
+                                                                                 name="password_confirmation" required
+                                                                                 class="input" name="password"
+                                                                                 placeholder="Wachtwoord"></input></p>
                 </div>
-                <button class="button2" name="submit" >Create</button>
+                <button class="button2" name="submit">Create</button>
             </form>
-            </div>
         </div>
-    </section>
+    </div>
+</section>
 
-  </body>
-    </html>
+</body>
+</html>
