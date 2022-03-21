4fef2cb553f5335da3b93f5f2af06e7c1fe4ca3f
JuiceWrld12334124 <vgeance50@gmail.com>
2022-03-21 09:12:56 +0100

    Preloader added

--- website - posts/Posts-Page.php
+++ website - posts/Posts-Page.php
@@ -18,6 +18,9 @@
     <link rel="stylesheet" href="/css/Framework.css">
     <title>Document</title>
 </head>
+<div class="loader">
+      <img id="s-width" src="img/943d662b-25c9-42d6-9fd2-cc36d2ffab76.gif" alt="">
+</div>
     <!--nav-bar-->
     <div class="navbar sticky">
         <div class="container flex">
